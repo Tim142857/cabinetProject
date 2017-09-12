130fe28a1b4fa3e67f610a21336bc5e104d001f4
Tim142857 <timothyleroch@gmail.com>
2017-09-12 12:37:06 +0200

    wsip

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,34 @@
 namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $name = $request->request->get('name');
+        $firstName = $request->request->get('firstName');
+        $phone = $request->request->get('phone');
+        $mail = $request->request->get('mail');
+        $message = $request->request->get('message');
+        if ($name != null || $firstName != null || $phone != null || $mail != null || $message != null) {
+            $transporter = \Swift_SmtpTransport::newInstance('173.194.65.108', 465, 'ssl')
+                ->setUsername('timothyleroch@gmail.com')
+                ->setPassword('#Google142857');
+
+            $mailer = \Swift_Mailer::newInstance($transporter);
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('sujet')
+                ->setFrom('timothyleroch@gmail.com')
+                ->setTo('timothyleroch@gmail.com')
+                ->setBody('toto');
+
+            $mailer->send($message);
+
+            return $this->render('CoreBundle::index.html.twig');
+        }
         return $this->render('CoreBundle::index.html.twig');
     }
 }
